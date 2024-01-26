82313f23ffcbace5044598f7f2852bcc2529816d
harshmetha9 <harshmehta8530@gmail.com>
2024-01-27 00:25:03 +0530

    Second Commit

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const listItems = document.getElementById('list-items');
 const addUpdateClick = document.getElementById("addUpdateClick");
 let updateText;
 let updateTextParent;
-
+let todoData = JSON.parse(localStorage.getItem(todoData));
+if(!todoData){
+    todoData = [];
+}
 
 
 todoValue.addEventListener('keypress', function(e){
@@ -23,19 +26,30 @@ function createTodoData(){
     const todoItem = `<div ondblclick="completeTodoItem(this)">${todoValue.value}</div><div><img src="./images/edit.png" onclick="updateTodoItem(this)" class="edit todo-controls"> <img class="delete todo-controls"  onclick="deleteTodoItem(this)" src="./images/delete.png"></div>`;
     li.innerHTML = (todoItem);
     listItems.appendChild(li);
-    todoValue.value = ""
+    todoValue.value = "";
+
+    if(!todoData){
+        todoData: [];
+    }
+
+    let dataItem = {
+        item: todoValue.value,
+        status: false
+    }
+    console.log(dataItem);
+    todoData.push(dataItem);
 }
 
 function completeTodoItem(e){
     console.log(e);
     if(e.parentElement.querySelector("div").style.textDecoration === ""){
-        e.parentElement.querySelector("div").style.textDecoration = "line-through"
+        e.parentElement.querySelector("div").style.textDecoration = "line-through";
     }
 }
 
 function updateSelectionItems(){
     updateText.innerHTML = todoValue.value;
-    addUpdateClick.innerHTML = "+"
+    addUpdateClick.innerHTML = "&#128077;"
     todoValue.value = ""
     updateTextParent.style.border = "none"
     
@@ -49,7 +63,7 @@ function updateTodoItem(e){
     updateTextParent = e.parentElement.parentElement;
     console.log(updateTextParent);
     updateTextParent.style.border = "1px solid limegreen";
-    addUpdateClick.innerHTML = "&#8634";
+    addUpdateClick.innerHTML = "&#9997;"
     }
 }
 
