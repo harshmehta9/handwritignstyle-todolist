ff369fd4f4aa9667eb5827e7e588a084ef72ca0e
harsh1154 <harshmehta4951@gmail.com>
2024-01-26 16:00:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+let todoValue = document.getElementById('todoText');
+const listItems = document.getElementById('list-items');
+const addUpdateClick = document.getElementById("addUpdateClick");
+let updateText;
+let updateTextParent;
+
+
+
+todoValue.addEventListener('keypress', function(e){
+    if(e.key === "Enter"){
+        addUpdateClick.click();
+    }
+})
+
+function createTodoData(){
+    // alert(todoValue.value)
+    if(todoValue.value === ""){
+        alert("Todo can't be Noting!")
+        todoValue.focus();
+    }
+
+    let li = document.createElement("li");
+    const todoItem = `<div ondblclick="completeTodoItem(this)">${todoValue.value}</div><div><img src="./images/edit.png" onclick="updateTodoItem(this)" class="edit todo-controls"> <img class="delete todo-controls"  onclick="deleteTodoItem(this)" src="./images/delete.png"></div>`;
+    li.innerHTML = (todoItem);
+    listItems.appendChild(li);
+    todoValue.value = ""
+}
+
+function completeTodoItem(e){
+    console.log(e);
+    if(e.parentElement.querySelector("div").style.textDecoration === ""){
+        e.parentElement.querySelector("div").style.textDecoration = "line-through"
+    }
+}
+
+function updateSelectionItems(){
+    updateText.innerHTML = todoValue.value;
+    addUpdateClick.innerHTML = "+"
+    todoValue.value = ""
+    updateTextParent.style.border = "none"
+    
+};
+
+function updateTodoItem(e){
+    if(e.parentElement.parentElement.querySelector("div").style.textDecoration === ""){
+    todoValue.value = e.parentElement.parentElement.querySelector("div").innerHTML;
+    addUpdateClick.setAttribute("onclick", "updateSelectionItems()");
+    updateText = e.parentElement.parentElement.querySelector("div");
+    updateTextParent = e.parentElement.parentElement;
+    console.log(updateTextParent);
+    updateTextParent.style.border = "1px solid limegreen";
+    addUpdateClick.innerHTML = "&#8634";
+    }
+}
+
+function deleteTodoItem(e){
+    let deleteValue = e.parentElement.parentElement.querySelector("div").innerHTML;
+    if(confirm("Are you sure you want to delete this?")){
+        e.parentElement.parentElement.parentElement.querySelector('li').remove();
+       
+    }
+}
